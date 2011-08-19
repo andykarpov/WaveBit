bfc0edde7d96baa4a627e37aaac50706b5d7cd07
Andrey Karpov <andy.karpov@gmail.com>
2011-08-20 01:23:31 +0300

    initial commit

---
+++ WavePinDefs.h
@@ -0,0 +1,32 @@
+/**
+ * \file
+ * Pin definitions
+ */
+#include <ArduinoPins.h>
+#ifndef WavePinDefs_h
+#define WavePinDefs_h
+
+//SPI pin definitions
+
+/** SPI slave select pin. Warning: SS may be redefined as another pin
+ but the hardware SS_PIN must be set to output mode before any calls to
+ WaveHC functions. The SS_PIN can then be used as a general output pin */
+#define SS   SS_PIN
+
+/** SPI master output, slave input pin. */
+#define MOSI MOSI_PIN
+
+/** SPI master input, slave output pin. */
+#define MISO MISO_PIN
+
+/** SPI serial clock pin. */
+#define SCK  SCK_PIN
+
+//------------------------------------------------------------------------------
+// DAC pin definitions
+
+// LDAC may be connected to ground to save a pin
+/** Set USE_MCP_DAC_LDAC to 0 if LDAC is grounded. */
+#define SPEAKER_PIN 3
+
+#endif // WavePinDefs_h
